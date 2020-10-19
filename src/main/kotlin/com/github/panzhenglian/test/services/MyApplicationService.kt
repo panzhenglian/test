9e9d30fced5a0bc6a5c915a8b51920fa67077ff4
GitHub Action <action@github.com>
2020-10-19 07:01:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/panzhenglian/test/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/panzhenglian/test/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.panzhenglian.test.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.panzhenglian.test.MyBundle
 
 class MyApplicationService {
 
